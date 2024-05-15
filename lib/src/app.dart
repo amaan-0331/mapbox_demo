1220bc9b35155b223219bf08c319f3c87dfddb37
amaan-honorfx <masteramaan0331@gmail.com>
2024-05-15 15:28:00 +0400

    add new marker/annotation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 class MainApp extends StatelessWidget {
@@ -12,15 +13,46 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({
     super.key,
   });
 
   @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  void _onMapCreated(MapboxMap mapboxMap) {
+    // create annotation manager to add markers/annotations
+    mapboxMap.annotations
+        .createPointAnnotationManager()
+        .then((pointAnnotationManager) async {
+      // load the marker icon
+      final bytes = await rootBundle.load('assets/marker_icon.png');
+      final list = bytes.buffer.asUint8List();
+
+      // create annotation manager and add the point
+      final point = PointAnnotationOptions(
+        geometry: Point(coordinates: Position(51.5286911, 25.3270487)).toJson(),
+        image: list,
+        iconSize: 0.25,
+      );
+      await pointAnnotationManager.create(point);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: MapWidget(),
+      child: MapWidget(
+        onMapCreated: _onMapCreated,
+        cameraOptions: CameraOptions(
+          center: Point(coordinates: Position(51.5286911, 25.3270487)).toJson(),
+          zoom: 15,
+          pitch: 50,
+        ),
+      ),
     );
   }
 }
